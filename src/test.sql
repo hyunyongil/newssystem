1362adea94dccfbbc61312b1a66b5f3eb959907e
hyr <cy56@ggook.com>
2022-04-25 14:48:35 +0800

    k_2

---
+++ src/test.sql
@@ -0,0 +1,47 @@
+
+07:31:08
+
+39 / 47:52
+
+150000
+338000
+-100000
+400000
+-101000
+375000
+-100000
+1009540
+05_02_02_09_00
+31884
+도매매 스피드고전송기 쇼피전송기 주문테스트입니다. 구매취소 부탁드립니다
+*/10 * * * * /usr/bin/php /home/domeme/crontab_pc/shopee_update_itemList.php
+
+28 1 * * * /usr/bin/php /home/domeme/crontab_pc/speedgo_smartstore_orderList.php
+28 3 * * * /usr/bin/php /home/domeme/crontab_pc/speedgo_coupang_orderList.php
+28 4 * * * /usr/bin/php /home/domeme/crontab_pc/speedgo_elevenst_orderList.php
+1 5 * * * /usr/bin/php /home/domeme/crontab_pc/speedgo_ssg_orderList.php
+29 5 * * * /usr/bin/php /home/domeme/crontab_pc/speedgo_lotteon_orderList.php
+59 5 * * * /usr/bin/php /home/domeme/crontab_pc/speedgo_wmp_orderList.php
+
+
+sellgood21-> Insert:1, Update:0, Cancel:0 ____________________2009 / 1484
+sns@384471-> Insert:7, Update:0, Cancel:0 ____________________2009 / 1485
+sns@386323-> Insert:1, Update:0, Cancel:0 ____________________2009 / 1486
+zxc1004i-> 
+
+
+Array
+(
+    [aid] => c63767813fe245e436004f87550015ff
+    [ver] => 4.3
+    [mode] => setOrder
+    [id] => chinaggook
+    [sId] => 62047b9f170ba07f0ce5b94b
+    [ie] => utf-8
+    [oe] => utf-8
+    [om] => json
+    [alliance] => Shopee
+    [receipt] => 1
+    [item[20317766]] => supply||P||00|1||https://domemedb.domeggook.com/index/shopeeApi/shopee/waybill_file/202202/2202104M9VBPMJ54451018.pdf||
+    [deliinfo] => 차이나꾹||07328|서울 영등포구 국제금융로6길 30 (여의도동, 백상빌딩)|5층|010-1234-1234||쇼핑몰명입니다2|
+)
