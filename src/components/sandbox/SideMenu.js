719914fb90685b70c1c66a48a8dc77477fc28c77
xyr <cy56@ggook.com>
2022-04-27 20:56:04 +0800

    k12

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Menu } from 'antd'
+import { withRouter } from 'react-router-dom'
 import {
   UserOutlined,
   VideoCameraOutlined,
@@ -13,7 +14,7 @@ const menuList = [
   {
     key: "/home",
     title: "首页",
-    icon: <UserOutlined />
+    icon: <VideoCameraOutlined />
   },
   {
     key: "/user-manage",
@@ -30,28 +31,41 @@ const menuList = [
   {
     key: "/right-manage",
     title: "权限管理",
-    icon: <UserOutlined />,
+    icon: <UploadOutlined />,
     children: [
       {
         key: "/right-manage/role/list",
         title: "角色列表",
-        icon: <UserOutlined />
+        icon: <UploadOutlined />
       },
       {
         key: "/right-manage/right/list",
         title: "权限列表",
-        icon: <UserOutlined />
+        icon: <UploadOutlined />
       }
     ]
   }
 ]
 
-export default function SideMenu() {
+function SideMenu(props) {
+  const renderMenu = (menuList) => {
+    return menuList.map(item => {
+      if (item.children) {
+        return <SubMenu key={item.key} icon={item.icon} title={item.title}>
+          {renderMenu(item.children)}
+        </SubMenu>
+      } else {
+        return <Menu.Item key={item.key} icon={item.icon} onClick={() => {
+          props.history.push(item.key)
+        }}>{item.title}</Menu.Item>
+      }
+    })
+  }
   return (
     <Sider trigger={null} collapsible collapsed={false}>
       <div className="logo">全球新闻发布管理</div>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
+        {/* <Menu.Item key="1" icon={<UserOutlined />}>
           首页
         </Menu.Item>
         <Menu.Item key="2" icon={<VideoCameraOutlined />}>
@@ -65,8 +79,11 @@ export default function SideMenu() {
           <Menu.Item key="10">Option10</Menu.Item>
           <Menu.Item key="11">Option11</Menu.Item>
           <Menu.Item key="12">Option12</Menu.Item>
-        </SubMenu>
+        </SubMenu> */}
+        {renderMenu(menuList)}
       </Menu>
     </Sider>
   )
 }
+
+export default withRouter(SideMenu)
--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,10 +1,22 @@
 import React, { PureComponent } from 'react'
 import { Button } from 'antd'
+import axios from 'axios'
 export default class Home extends PureComponent {
   render() {
+    const ajax = () => {
+      //取数据
+      axios.get("http://localhost:8000/posts/4").then(res => {
+        console.log(res.data)
+      })
+      //增
+      // axios.post("http://localhost:8000/posts", {
+      //   title: "444",
+      //   author: "新来的44"
+      // })
+    }
     return (
       <div>
-          <Button type="primary">Button</Button>
+        <Button type="primary" onClick={ajax}>Button</Button>
       </div>
     )
   }
