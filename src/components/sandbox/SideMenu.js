7a962316486736d00e63720d66aeea84cc118261
xyr <cy56@ggook.com>
2022-04-28 20:28:26 +0800

    k14

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,63 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd'
 import { withRouter } from 'react-router-dom'
 import {
   UserOutlined,
   VideoCameraOutlined,
   UploadOutlined,
+  CopyOutlined,
+  FormOutlined,
+  SoundOutlined
 } from '@ant-design/icons';
 import './index.css'
+import axios from 'axios';
 const { Sider } = Layout
 const { SubMenu } = Menu
 
-const menuList = [
-  {
-    key: "/home",
-    title: "首页",
-    icon: <VideoCameraOutlined />
-  },
-  {
-    key: "/user-manage",
-    title: "用户管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/user-manage/list",
-        title: "用户管理",
-        icon: <UserOutlined />
-      }
-    ]
-  },
-  {
-    key: "/right-manage",
-    title: "权限管理",
-    icon: <UploadOutlined />,
-    children: [
-      {
-        key: "/right-manage/role/list",
-        title: "角色列表",
-        icon: <UploadOutlined />
-      },
-      {
-        key: "/right-manage/right/list",
-        title: "权限列表",
-        icon: <UploadOutlined />
-      }
-    ]
-  }
-]
+// const menuList = [
+//   {
+//     key: "/home",
+//     title: "首页",
+//     icon: <VideoCameraOutlined />
+//   },
+//   {
+//     key: "/user-manage",
+//     title: "用户管理",
+//     icon: <UserOutlined />,
+//     children: [
+//       {
+//         key: "/user-manage/list",
+//         title: "用户管理",
+//         icon: <UserOutlined />
+//       }
+//     ]
+//   },
+//   {
+//     key: "/right-manage",
+//     title: "权限管理",
+//     icon: <UploadOutlined />,
+//     children: [
+//       {
+//         key: "/right-manage/role/list",
+//         title: "角色列表",
+//         icon: <UploadOutlined />
+//       },
+//       {
+//         key: "/right-manage/right/list",
+//         title: "权限列表",
+//         icon: <UploadOutlined />
+//       }
+//     ]
+//   }
+// ]
 
+const iconList = {
+  "/home": <VideoCameraOutlined />,
+  "/user-manage": <UserOutlined />,
+  "/user-manage/list": <UserOutlined />,
+  "/right-manage": <UploadOutlined />,
+  "/right-manage/role/list": <UploadOutlined />,
+  "/right-manage/right/list": <UploadOutlined />,
+  "/news-manage": <CopyOutlined />,
+  "/audit-manage": <FormOutlined />,
+  "/publish-manage": <SoundOutlined />
+}
 function SideMenu(props) {
+  const [meun, setMeun] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:8000/rights?_embed=children").then(res => {
+      console.log(res.data)
+      setMeun(res.data)
+    })
+  }, []);
+
+  const checkPagePermission = (item) => {
+    return item.pagepermisson === 1
+  }
+
   const renderMenu = (menuList) => {
     return menuList.map(item => {
-      if (item.children) {
-        return <SubMenu key={item.key} icon={item.icon} title={item.title}>
+      if (item.children && checkPagePermission(item)) {
+        return <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
           {renderMenu(item.children)}
         </SubMenu>
       } else {
-        return <Menu.Item key={item.key} icon={item.icon} onClick={() => {
+        return checkPagePermission(item) && <Menu.Item key={item.key} icon={iconList[item.key]} onClick={() => {
           props.history.push(item.key)
-        }}>{item.title}</Menu.Item>
+        }
+        }> {item.title}</Menu.Item >
       }
     })
   }
@@ -80,7 +108,7 @@ function SideMenu(props) {
           <Menu.Item key="11">Option11</Menu.Item>
           <Menu.Item key="12">Option12</Menu.Item>
         </SubMenu> */}
-        {renderMenu(menuList)}
+        {renderMenu(meun)}
       </Menu>
     </Sider>
   )
--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -5,14 +5,36 @@ export default class Home extends PureComponent {
   render() {
     const ajax = () => {
       //取数据
-      axios.get("http://localhost:8000/posts/4").then(res => {
-        console.log(res.data)
-      })
+      // axios.get("http://localhost:8000/posts/4").then(res => {
+      //   console.log(res.data)
+      // })
+
       //增
       // axios.post("http://localhost:8000/posts", {
       //   title: "444",
       //   author: "新来的44"
       // })
+
+      //修改替换 put
+      // axios.put("http://localhost:8000/posts/4", {
+      //   title: "1111-修改"
+      // })
+      //修改 patch
+      // axios.patch("http://localhost:8000/posts/4", {
+      //   title: "1111-修改"
+      // })
+
+      //删除 delete
+      //axios.delete("http://localhost:8000/posts/2")
+
+      //关联查询 向下 _embed
+      // axios.get("http://localhost:8000/posts?_embed=comments").then(res => {
+      //   console.log(res.data)
+      // })
+      //关联查询 向上 _expand
+      axios.get("http://localhost:8000/comments?_expand=post").then(res => {
+        console.log(res.data)
+      })
     }
     return (
       <div>
