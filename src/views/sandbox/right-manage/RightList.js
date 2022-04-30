1129cf512c4e85653e2b5ede930c890cd1bfc367
xyr <cy56@ggook.com>
2022-04-30 08:35:09 +0800

    k17

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,21 +1,29 @@
-import { Table } from 'antd'
+import { Table, Tag, Button } from 'antd'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
+import {
+  DeleteOutlined,
+  EditOutlined
+} from '@ant-design/icons';
 export default function RightList() {
   const [dataSource, setdataSource] = useState([])
   // fetch("http://localhost:8000/rights", "get").then(res => {
   //   setdataSource(res.data)
   // })
   useEffect(() => {
-    axios.get("http://localhost:8000/rights").then(res => {
+    axios.get("http://localhost:8000/rights?_embed=children").then(res => {
+      const list = res.data
+      list[0].children = ""
       setdataSource(res.data)
     })
   }, [])
   const columns = [
     {
       title: 'ID',
-      dataIndex: 'id'
+      dataIndex: 'id',
+      render: (id) => {
+        return <b>{id}</b>
+      }
     },
     {
       title: '权限名称',
@@ -23,16 +31,28 @@ export default function RightList() {
     },
     {
       title: '权限路径',
-      dataIndex: 'key'
+      dataIndex: 'key',
+      render: (key) => {
+        return <Tag color="volcano">{key}</Tag>
+      }
     },
     {
       title: '操作',
-      dataIndex: 'pagepermisson'
+      render: () => {
+        return <div>
+          <Button danger shape="circle" icon={<DeleteOutlined />} />
+          <Button type="primary" shape="circle" icon={<EditOutlined />} style={{ marginLeft: 10 }} />
+        </div>
+      }
     }
   ]
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} />
+      <Table dataSource={dataSource} columns={columns}
+        pagination={{
+          pageSize: 5,
+          position: ["bottomCenter"]
+        }} />
     </div>
   )
 }
