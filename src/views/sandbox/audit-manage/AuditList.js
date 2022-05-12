9581efcdceda492c9bd0d3fb7745d97d656b6a5b
xyr <cy56@ggook.com>
2022-05-12 20:30:07 +0800

    k48

--- src/views/sandbox/audit-manage/AuditList.js
+++ src/views/sandbox/audit-manage/AuditList.js
@@ -1,7 +1,77 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Table, Button, Tag } from 'antd'
+import axios from 'axios'
+import {
+    SendOutlined,
+    ClearOutlined,
+    AppleOutlined
+} from '@ant-design/icons';
 export default function AuditList() {
+    const [dataSource, setdataSource] = useState([])
+    const { username } = JSON.parse(localStorage.getItem("token"))
+    useEffect(() => {
+        axios.get(`/news?auditState_ne=0&author_new=${username}&publishState_lte=1&_expand=category`).then(res => {
+            const list = res.data
+            setdataSource(list)
+        })
+    }, [username])
+
+    const columns = [
+        {
+            title: '新闻标题',
+            dataIndex: 'title',
+            render: (title, item) => {
+                return <a href={`#/news-manage/preview/${item.id}`}>{title}</a>
+            }
+        },
+        {
+            title: '作者',
+            dataIndex: 'author'
+        },
+        {
+            title: '新闻分类',
+            dataIndex: 'category',
+            render: (category) => {
+                return <div>{category.title}</div>
+            }
+        },
+        {
+            title: '审核状态',
+            dataIndex: 'auditState',
+            render: (auditState) => {
+                const colorList = ["", "orange", "green", "red"]
+                const auditList = ["草稿箱", "审核中", "已通过", "未通过"]
+                return <Tag color={colorList[auditState]}>{auditList[auditState]}</Tag>
+            }
+        },
+        {
+            title: '操作',
+            render: (item) => {
+                return <div>
+                    {
+                        item.auditState === 1 && <Button icon={<ClearOutlined />}>撤销</Button>
+                    }
+                    {
+                        item.auditState === 2 && <Button type='danger' icon={<SendOutlined />} >发布</Button>
+                    }
+
+                    {
+                        item.auditState === 3 && <Button type='primary' icon={<AppleOutlined />}>更新</Button>
+                    }
+
+                </div>
+            }
+        }
+    ]
+
+
     return (
-        <div>AuditList</div>
+        <div>
+            <Table dataSource={dataSource} columns={columns}
+                pagination={{
+                    pageSize: 5,
+                    position: ["bottomCenter"]
+                }} rowKey={item => item.id} />
+        </div>
     )
 }
--- src/views/sandbox/news-manage/NewsDraft.js
+++ src/views/sandbox/news-manage/NewsDraft.js
@@ -1,4 +1,4 @@
-import { Table, Button, Modal } from 'antd'
+import { Table, Button, Modal, notification } from 'antd'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import {
@@ -37,7 +37,7 @@ export default function NewsDraft(props) {
             dataIndex: 'author'
         },
         {
-            title: '分类',
+            title: '新闻分类',
             dataIndex: 'category',
             render: (category) => {
                 return category.title
@@ -51,12 +51,26 @@ export default function NewsDraft(props) {
                     <Button shape="circle" icon={<EditOutlined />} style={{ marginLeft: 5 }} onClick={() => {
                         props.history.push(`/news-manage/update/${item.id}`)
                     }} />
-                    <Button type="primary" shape="circle" icon={<UploadOutlined />} style={{ marginLeft: 5 }} />
+                    <Button type="primary" shape="circle" icon={<UploadOutlined />} style={{ marginLeft: 5 }} onClick={() => handleCheck(item.id)} />
                 </div>
             }
         }
     ]
 
+    const handleCheck = (id) => {
+        axios.patch(`/news/${id}`, {
+            auditState: 1
+        }).then(res => {
+            props.history.push('/audit-manage/list')
+
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到审核列表中查看您的新闻`,
+                placement: "bottomRight"
+            });
+        })
+    }
     const confirmMethod = (item) => {
         confirm({
             title: '你确定要删除吗?',
