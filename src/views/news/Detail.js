5762bfdaabc0ee102683e9132992ec87d056eba1
xyr <cy56@ggook.com>
2022-05-21 09:14:42 +0800

    k67

--- src/views/news/Detail.js
+++ src/views/news/Detail.js
@@ -1,7 +1,72 @@
-import React from 'react'
-
-export default function Detail() {
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { PageHeader, Descriptions, message } from 'antd'
+import { HeartTwoTone } from '@ant-design/icons'
+import moment from 'moment'
+//cnpm i --save moment
+export default function Detail(props) {
+    const [newsInfo, setnewsInfo] = useState(null)
+    const [heartState, setheartState] = useState(true)
+    useEffect(() => {
+        axios.get(`/news/${props.match.params.id}?_expand=category&_expand=role`).then(res => {
+            setnewsInfo({
+                ...res.data,
+                view: res.data.view + 1
+            })
+            //同步后端
+            return res.data
+        }).then(res => {
+            axios.patch(`/news/${props.match.params.id}`, {
+                view: res.view + 1
+            })
+        })
+    }, [props.match.params.id])
+    const handleStar = () => {
+        if (!heartState) {
+            message.warning("已点赞~!")
+        }
+        setnewsInfo({
+            ...newsInfo,
+            star: newsInfo.star + 1
+        })
+        axios.patch(`/news/${props.match.params.id}`, {
+            star: newsInfo.star + 1
+        }).then(res => {
+            setheartState(false)
+        })
+    }
     return (
-        <div>Detail</div>
+        <div style={{ width: "95%", margin: '0 auto' }}>
+            {
+                newsInfo && <div>
+                    <PageHeader
+                        onBack={() => window.history.back()}
+                        title={newsInfo.title}
+                        subTitle={
+                            <div>
+                                {newsInfo.category.title}
+                                <HeartTwoTone style={{ marginLeft: "5px" }} twoToneColor="#eb2f96" onClick={() => handleStar()} />
+                            </div>
+                        }>
+                        <Descriptions size="small" column={3}>
+                            <Descriptions.Item label="创建者">{newsInfo.author}</Descriptions.Item>
+                            <Descriptions.Item label="发布时间">{newsInfo.publishTime ? moment(newsInfo.publishTime).format("YYYY/MM/DD HH:mm:ss") : "-"}</Descriptions.Item>
+                            <Descriptions.Item label="区域">{newsInfo.region}</Descriptions.Item>
+                            <Descriptions.Item label="访问数量">{newsInfo.view}</Descriptions.Item>
+                            <Descriptions.Item label="点赞数量">{newsInfo.star}</Descriptions.Item>
+                            <Descriptions.Item label="评论数量">0</Descriptions.Item>
+                        </Descriptions>
+                    </PageHeader>
+                    <div dangerouslySetInnerHTML={{
+                        __html: newsInfo.content
+                    }} style={{
+                        border: "1px solid #ccc",
+                        padding: "15px",
+                        margin: "0 24px"
+                    }}>
+                    </div>
+                </div>
+            }
+        </div >
     )
 }
--- src/views/news/News.js
+++ src/views/news/News.js
@@ -1,7 +1,47 @@
-import React from 'react'
-
+import { PageHeader, Col, Row, Card, List } from 'antd'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import _ from 'lodash'
 export default function News() {
+    const [list, setlist] = useState([])
+    useEffect(() => {
+        axios.get(`/news?publishState2&_expand=category`).then(res => {
+            setlist(Object.entries(_.groupBy(res.data, item => item.category.title)));
+        })
+    }, [])
     return (
-        <div>News</div>
+        <div style={{ width: "95%", margin: '0 auto' }}>
+            <PageHeader
+                className="site-page-header"
+                title="全球大新闻"
+                subTitle="查看新闻"
+            />
+            <div className="site-card-wrapper">
+                <Row gutter={[20, 20]}>
+                    {
+                        list.map(item =>
+                            <Col span={8} key={item[0]}>
+                                <Card title={item[0]} bordered={true} hoverable={true}>
+                                    <List
+                                        size="small"
+                                        dataSource={item[1]}
+                                        pagination={{
+                                            pageSize: 3,
+                                            position: "bottom",
+                                            style: {
+                                                textAlign: "center",
+                                                width: "100%"
+                                            }
+                                        }}
+                                        renderItem={data => <List.Item><a href={`#/detail/${data.id}`}>{data.title}</a></List.Item>}
+                                    />
+                                </Card>
+                            </Col>
+                        )
+                    }
+
+                </Row>
+            </div>
+        </div>
     )
 }
